4b60db83c65a035580948ff27057192525baeed5
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-02-13 19:40:05 +0000

    update

--- MyPiControlNonAndroidFiles/sender_and_receiver_LED.py
+++ MyPiControlNonAndroidFiles/sender_and_receiver_LED.py
@@ -6,7 +6,7 @@ from grovepi import *
 
 from threading import Thread
 
-buzzer_pin = 2		#Port for buzzer
+buzzer_pin = 2  # Port for buzzer
 led = 5
 dht_sensor_port = 7  # Connect the DHt sensor to port D7
 # Leave at 0 if using blue-coloured sensor, change to 1 if using white coloured sensor
@@ -14,13 +14,14 @@ dht_sensor_type = 0
 light_sensor = 1  # Connect light sensor to A1
 ultrasonic_ranger = 4  # Connect ultrasonic sensor to port D4
 
-pinMode(buzzer_pin,"OUTPUT")	# Assign mode for buzzer as output
-grovepi.pinMode(led,"OUTPUT")
+pinMode(buzzer_pin, "OUTPUT")  # Assign mode for buzzer as output
+grovepi.pinMode(led, "OUTPUT")
+
 
 def getTemperature():  # Function that returns temperature value from the sensor
     [temp, hum] = dht(dht_sensor_port, dht_sensor_type)
     # if isnan(temp) is True:
-    #raise TypeError('The value returned from temperature sensor is not a number,this could mean the sensor is plugged out from port D7')
+    # raise TypeError('The value returned from temperature sensor is not a number,this could mean the sensor is plugged out from port D7')
     time.sleep(1)
     return temp
 
@@ -28,21 +29,25 @@ def getTemperature():  # Function that returns temperature value from the sensor
 def getHumidity():  # Function that returns humidity value from the sensor
     [temp, hum] = dht(dht_sensor_port, dht_sensor_type)
     # if isnan(temp) is True:
-    #raise TypeError('The value returned from humidity sensor is not a number,this could mean the sensor is plugged out from port D7')
+    # raise TypeError('The value returned from humidity sensor is not a number,this could mean the sensor is plugged out from port D7')
     time.sleep(1)
     return hum
+
 
 def getUltrasonic():  # Function that gets ultrasonic distance and turns on the relay if distance = 30 or less, otherwise relay is off
     time.sleep(1)
     distance = ultrasonicRead(ultrasonic_ranger)
     return distance  # Returning distance of the relay
 
+
 def getLight():
     time.sleep(1)
     lightValue = grovepi.analogRead(light_sensor)
     return lightValue
 
+
 publisher_state = False
+
 
 def listener(publisher):
     for dweet in dweepy.listen_for_dweets_from('mypicontrolboard'):
@@ -59,19 +64,18 @@ def listener(publisher):
         else:
             publisher_state = False
             print "wasn't true"
-    
+
+
 def publisher_method_dan():
     while publisher_state:
-        #result = dweepy.dweet_for('mypicontrolboard', {"LEDStatus": "true"})
-        grovepi.analogWrite(led,1000/4)
-       # print result
+        # result = dweepy.dweet_for('mypicontrolboard', {"LEDStatus": "true"})
+        grovepi.analogWrite(led, 1000 / 4)
+        # print result
         time.sleep(1)
     print "publishing ending"
-    grovepi.analogWrite(led,0/4)
-    
+    grovepi.analogWrite(led, 0 / 4)
 
-    
+
 publisher_thread = Thread(target=publisher_method_dan)
 listener_thread = Thread(target=listener, args=(publisher_thread,))
 listener_thread.start()
- 
