7029bcf432c46a5e30749201ca831382b4cf97c4
Vlad Vasiljev <vlad.vasiljevs97@gmail.com>
2019-02-14 02:44:52 +0000

    sample rate change on android and python script done

--- app/src/main/java/com/example/mypicontrol/buzzerControl.java
+++ app/src/main/java/com/example/mypicontrol/buzzerControl.java
@@ -1,9 +1,10 @@
 package com.example.mypicontrol;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +20,9 @@ public class buzzerControl extends AppCompatActivity implements View.OnClickList
     private RequestQueue mQueue;
     private TextView view;
     public static String getBuzzerStatusValue;
+    TextView tvProgressLabel;
+    int finalprogress;
+
     //Class that controls the buzzer simple on and off function
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +31,20 @@ public class buzzerControl extends AppCompatActivity implements View.OnClickList
 
         Button offButton = findViewById(R.id.offButton);
         Button onButton = findViewById(R.id.onButton);
+        Button sampleRateButton = findViewById(R.id.updateSampleRate);
 
-        offButton.setOnClickListener(this);
-        onButton.setOnClickListener(this);
+        offButton.setOnClickListener(this);//listener for on button
+        onButton.setOnClickListener(this);//listener for off button
+        sampleRateButton.setOnClickListener(this);//listener for sample rate button
+
+        //Seekbar was implemented using this https://stackoverflow.com/questions/8629535/implementing-a-slider-seekbar-in-android
+        // set a change listener on the SeekBar
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(seekBarChangeListener);
+        int progress = seekBar.getProgress();
+        tvProgressLabel = findViewById(R.id.progressTxt);
+        tvProgressLabel.setText("Progress: " + progress);
+
 
         view = findViewById(R.id.textView);
 
@@ -37,14 +52,38 @@ public class buzzerControl extends AppCompatActivity implements View.OnClickList
                 .getRequestQueue();
 
     }
-    String LEDStatusValue = LEDControl.getLEDStatusValue;
+
+    //https://stackoverflow.com/questions/8629535/implementing-a-slider-seekbar-in-android
+    SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            // updated continuously as the user slides the thumb
+            tvProgressLabel.setText("Progress: " + progress);
+            finalprogress = progress;
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // called when the user first touches the SeekBar
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // called after the user finishes moving the SeekBar
+        }
+    };
+
+
+    String LEDStatusValue = LEDControl.getLEDStatusValue;//Getting LEDStatus value from ledcontrol class
+
     @Override
     public void onClick(View view) {
 
         switch (view.getId()) {
 
             case R.id.offButton:
-                String url = "https://dweet.io/dweet/for/mypicontrolboard?BuzzerStatus=false&LEDStatus="+LEDStatusValue;
+                String url = "https://dweet.io/dweet/for/mypicontrolboard?BuzzerStatus=false&LEDStatus=" + LEDStatusValue + "&SampleRate=0";
                 CustomJSONRequest jsonRequest = new CustomJSONRequest(Request.Method.GET, url,
                         new JSONObject(), this, this);
                 //jsonRequest.setTag("test");
@@ -53,10 +92,19 @@ public class buzzerControl extends AppCompatActivity implements View.OnClickList
                 break;
 
             case R.id.onButton:
-                url = "https://dweet.io/dweet/for/mypicontrolboard?BuzzerStatus=true&LEDStatus="+LEDStatusValue;
+                url = "https://dweet.io/dweet/for/mypicontrolboard?BuzzerStatus=true&LEDStatus=" + LEDStatusValue + "&SampleRate=0";
                 jsonRequest = new CustomJSONRequest(Request.Method.GET, url,
                         new JSONObject(), this, this);
-               // jsonRequest.setTag("test");
+                // jsonRequest.setTag("test");
+                mQueue.add(jsonRequest);
+                getBuzzerStatusValue = "true";
+                break;
+
+                case R.id.updateSampleRate:
+                url = "https://dweet.io/dweet/for/mypicontrolboard?BuzzerStatus=true&LEDStatus=" + LEDStatusValue + "&SampleRate=" +finalprogress ;
+                jsonRequest = new CustomJSONRequest(Request.Method.GET, url,
+                        new JSONObject(), this, this);
+                // jsonRequest.setTag("test");
                 mQueue.add(jsonRequest);
                 getBuzzerStatusValue = "true";
                 break;
@@ -64,7 +112,6 @@ public class buzzerControl extends AppCompatActivity implements View.OnClickList
             default:
                 break;
         }
-
     }
 
     @Override
@@ -74,12 +121,9 @@ public class buzzerControl extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onResponse(Object response) {
-        Toast.makeText(getApplicationContext(),response.toString(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
         view.setText(response.toString());
     }
-
-
-
 
 
 }
